2655fbd92af14aeb4bef26254200e99306ee1985
davidfajardotorres777 <davidfajardotorres7@gmail.com>
2026-07-17 11:32:13 -0300

    Add db_models/muestra.py

---
+++ db_models/muestra.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+
+class Muestra(BaseModel):
+    id: str = Field(alias="_id", default=None)
+    paciente_id: str
+    tipo_muestra: str # Ej: "Sangre", "Saliva", "Tejido Tumoral"
+    fecha_extraccion: datetime
+    medico_solicitante: str
+    estado: str = "Recolectada" # Recolectada, En Tránsito, Procesada, Analizada, Descartada
+    temperatura_almacenamiento: Optional[float] = None
+    notas: Optional[str] = None
+    
+    class Config:
+        populate_by_name = True
