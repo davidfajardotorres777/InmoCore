57fa46b45007633844a8fee4048b05a817b94e83
davidfajardotorres777 <davidfajardotorres7@gmail.com>
2026-07-17 11:37:51 -0300

    Update config_vars.py

--- config_vars.py
+++ config_vars.py
@@ -6,3 +6,7 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "biotrace_db")
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "localhost:9000")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "admin")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "password")
+MINIO_SECURE = os.getenv("MINIO_SECURE", "False").lower() in ("true", "1", "t")
